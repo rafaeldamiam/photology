1f2e35b33735a3801ad8a36f4d7fb40bdbbb9f23
rafaeldamiam <rafael.damiam23@gmail.com>
2019-08-07 08:57:26 -0300

    update view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Você está conectado!!!
                 </div>
             </div>
         </div>
